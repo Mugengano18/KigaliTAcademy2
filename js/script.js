64afff01a94b5bf661f01a91a0628937026fc430
Mugengano18 <mugenganoa@gmail.com>
2020-06-05 02:44:02 +0200

    I finished the first media queries

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+$(document).ready(function(){
+    $("#regiter").click(function(){
+        alert("Successfully Registered..")
+    })
+    $(".btn1").on("click", function () {
+        $('.menu').toggleClass("show");
+    })
+
+})
+
 function openForm() {
     document.getElementById("myForm").style.display = "block";
 }
@@ -5,30 +15,3 @@ function openForm() {
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
-$(document).ready(function(){
-    $("#data").click(function(){
-        alert("thank you for registering,We will review your cv and Get back at you.")
-    })
-    $("#suggest").click(function(){
-        alert("We highly appreciate your suggestion!!!")
-    })
-    $(".btn1").on("click", function () {
-        $('.menu').toggleClass("show");
-    })
-
-})
-$(document).ready(function(){
-    $(".click1").click(function(){
-        $("#par1").toggle();
-        $(".hidden1").toggle();
-    });
-    $(".click2").click(function(){
-        $("#par2").toggle();
-        $(".hidden2").toggle();
-    });
-
-    $(".click3").click(function(){
-        $("#par3").toggle();
-        $(".hidden3").toggle();
-    });
-})
